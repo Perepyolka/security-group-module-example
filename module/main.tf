64e97a4a4e9a4254a86df95d7c20ba45d5273486
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-04-27 23:31:55 +0300

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,4 +1,29 @@
 module "vpcccac" {
-	source  = "affa6e6b3805.test-env.scalr.com/global/vpcccac/aws"
+	source  = "ba4affd88842.test-env.scalr.com/global/vpcccac/aws"
 	version = "2.14.0"
+	name = "divi-sg"
+  	vpc_id = var.vpc_id
+}
+	
+variable "vpc_id" {
+   default = "vpc-596aa03e"
+
+}
+variable "region" {
+  description = "AWS region to use"
+  default = "us-east-1"
+}
+
+
+variable "cidr_block" {
+    default = "10.0.0.0/24"
+    description = "The CIDR block for the VPC"
+}
+variable "instance_type" {
+	default = "t2.small"
+}
+
+variable "subnet_id" {
+  description = "VPC Subnet into which to launch your instance"
+  default     = "subnet-3d82d101"
 }
