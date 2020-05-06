b53102998276a046d1e07b14ee4810ab2bc78d36
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-05-06 09:43:02 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #      region  = "us-east-1"		
 #    }
 
-  resource "aws_security_group" "allow_tls" {
-  	name = "divi-sg"
-  	vpc_id = var.vpc_id
-  }
+#   resource "aws_security_group" "allow_tls" {
+#   	name = "divi-sg"
+#   	vpc_id = var.vpc_id
+#   }
