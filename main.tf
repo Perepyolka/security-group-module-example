8d7a0528b12dcee5865f719288fb0a6f37fd5866
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-05-13 11:12:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "aws" {		
-     version = "2.55.0"		
-   }
+# provider "aws" {		
+#      version = "2.55.0"		
+#    }
 
-resource "aws_security_group" "allow_tls" {
-     name = "divi-sg-1"
-     vpc_id = var.vpc_id
-}
+# resource "aws_security_group" "allow_tls" {
+#      name = "divi-sg-1"
+#      vpc_id = var.vpc_id
+# }
