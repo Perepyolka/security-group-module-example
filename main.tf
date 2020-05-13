6fa6f8f1ff61fb379c48798d1156e60e4a36e164
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-05-13 13:05:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_security_group" "allow_tls" {
-     name = "divi-sg-1"
-     vpc_id = var.vpc_id
-}
+# resource "aws_security_group" "allow_tls" {
+#      name = "divi-sg-1"
+#      vpc_id = var.vpc_id
+# }
 
-resource "digitalocean_droplet" "node1" {
-  name   = "1node1"
-  image  = "ubuntu-16-04-x64"
-  size   = "s-1vcpu-1gb"
-  region = "lon1"
-}
+# resource "digitalocean_droplet" "node1" {
+#   name   = "1node1"
+#   image  = "ubuntu-16-04-x64"
+#   size   = "s-1vcpu-1gb"
+#   region = "lon1"
+# }
