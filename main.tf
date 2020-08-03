2c180fc3a39132f25ed71ca4cd979a47b0ccb2a9
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-08-03 17:36:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@
 #      name = "divi-sg-1"
 #      vpc_id = var.vpc_id
 # }
-# module "security-group" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   name = "divi-sg-1"
-#   vpc_id = var.vpc_id
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  name = "divi-sg-1"
+  vpc_id = var.vpc_id
   
-#   tags = {
-#     "owner" = "v_didenchuk@scalr.com"
-#     }
-#   # insert the 2 required variables here
-# }
+  tags = {
+    "owner" = "v_didenchuk@scalr.com"
+    }
+  # insert the 2 required variables here
+}
 # resource "digitalocean_droplet" "node1" {
 #   name   = "1node1"
 #   image  = "ubuntu-16-04-x64"
