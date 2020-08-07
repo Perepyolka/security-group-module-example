1c5be82ed44d96c7699577ec9336bb2de09f34a4
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-08-07 13:13:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-# resource "aws_security_group" "allow_tls" {
-#      name = "divi-sg-1"
-#      vpc_id = var.vpc_id
-# }
-
 module "security-group" {
   source  = "terraform-aws-modules/security-group/aws"
   name = "divi-sg-1"
@@ -11,26 +6,14 @@ module "security-group" {
   tags = {
     "owner" = "v_didenchuk@scalr.com"
     }
-  # insert the 2 required variables here
 }
 
-# resource "null_resource" "null" {
 
-#   }
+output "sg_name" {
+  value = module.security-group.this_security_group_name
+}
 
-# terraform {
-#   required_providers {
-#     aws = {}
-#     null = {}
-#     azurerm = {}
-#   }
-# }
+variable "vpc_id" {
+   default = "vpc-596aa03e"
 
-
-
-# resource "digitalocean_droplet" "node1" {
-#   name   = "1node1"
-#   image  = "ubuntu-16-04-x64"
-#   size   = "s-1vcpu-1gb"
-#   region = "lon1"
-# }
+}
