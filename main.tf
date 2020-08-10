17fbf217fa3c843fc6a9f6ede858d2748f7dd11e
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-08-10 12:45:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  name = "divi-sg-3"
-  vpc_id = var.vpc_id
+# module "security-group" {
+#   source  = "terraform-aws-modules/security-group/aws"
+#   name = "divi-sg-3"
+#   vpc_id = var.vpc_id
   
-  tags = {
-    "owner" = "v_didenchuk@scalr.com"
-    }
-}
+#   tags = {
+#     "owner" = "v_didenchuk@scalr.com"
+#     }
+# }
 
-output "sg_name" {
-  description = "security group name"
-  value = module.security-group.this_security_group_name
-}
+# output "sg_name" {
+#   description = "security group name"
+#   value = module.security-group.this_security_group_name
+# }
 
-  output "sg_id" {
-  value = module.security-group.this_security_group_id
-    sensitive = true
-}
+#   output "sg_id" {
+#   value = module.security-group.this_security_group_id
+#     sensitive = true
+# }
   
-variable "vpc_id" {
-   default = "vpc-596aa03e"
+# variable "vpc_id" {
+#    default = "vpc-596aa03e"
 
-}
+# }
