b887dc82b2844efaeacab17ce8759d2224f65979
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-03 16:06:31 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+variable "target_scalr_environment" {
+type        = string
+description = "The environment to put the subscription workspace in"
+}
+variable "target_scalr_environment" {
+type        = string
+description = "The environment to put the subscription workspace in"
+}
