6437910c768d61e37d8dcb54d9a79061cf03f209
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-10 09:55:19 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-|
-
-  resource "null_resource" "target_scalr_environment" {
-type        = string
-description = "The environment to put the subscription workspace in"
-  
-}
-resource "null_resource" "target_scalr_environment" {
-type        = string
-description = "The environment to put the subscription workspace in"
-}
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+  }
