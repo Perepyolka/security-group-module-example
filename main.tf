477703cfd859f23e1bc003f7aa3a88b3723ab011
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-17 11:26:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ resource "null_resource" "example" {
     command = "sleep 30"
   }
   }
+
+module "security-group" {
+   source  = "terraform-aws-modules/security-group/aws"
+   name = "divi-sg-3"
+   vpc_id = var.vpc_id
+
+   tags = {
+     "owner" = "v_didenchuk@scalr.com"
+     }
+ }
+  
+variable "vpc_id" {}
