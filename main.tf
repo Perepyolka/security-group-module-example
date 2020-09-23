46cc7301684a7ee5080c6020408cded442e04477
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-23 17:54:02 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "sleep 30"
-  }
-  }
-
-module "security-group" {
-   source  = "terraform-aws-modules/security-group/aws"
-   name = "divi-sg-3"
-   vpc_id = var.vpc_id
-
-   tags = {
-     "owner" = "v_didenchuk@scalr.com"
-     }
- }
-  
-variable "vpc_id" {}
+module "shared-gke" {
+  source                 = "modules/security-group"
+}
