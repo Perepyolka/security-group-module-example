5d4f52c82e10ffbcdcd82904b2cb02852ae225b9
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-23 18:10:43 +0300

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -2,4 +2,15 @@ resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
+  
+    boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
 }
