d25523b40613044f2a0cb557b2cd4e290ba4dcce
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-23 18:09:58 +0300

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -2,8 +2,4 @@ resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
-
-  tags = {
-    "test-env-owner": "v_didenchuk@scalr.com"
-    }
 }
