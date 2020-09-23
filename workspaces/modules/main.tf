d222edda32a08ca29e52677bc1290abdb95d58a0
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-23 18:39:19 +0300

    Create main.tf

---
+++ workspaces/modules/main.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  
+    boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+network_interface {
+  network = google_compute_network.vpc_network.self_link
+
+}
+
+  }
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-terraform-network"
+  auto_create_subnetworks = "true"
+}
