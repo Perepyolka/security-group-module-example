b880ca0bf2ac87f8b3549907be5120fe573c86a5
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-23 19:33:10 +0300

    Update main.tf

--- workspaces/modules/security-group/main.tf
+++ workspaces/modules/security-group/main.tf
@@ -1,20 +1,3 @@
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-  
-    boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-network_interface {
-  network = google_compute_network.vpc_network.self_link
-
-}
-
-  }
 
 resource "google_compute_network" "vpc_network" {
   name                    = "my-terraform-network"
