27b43aec7d57b1630c9a6fd26ad6037de77bdf29
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-23 18:27:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 module "shared-gke" {
   source                 = "../../modules/security-group"
 }
+
+  
+provider "google" {
+  version = "~> 3.39.0"
+}
+
+provider "google-beta" {
+  version = "~> 3.39.0"
+}
