6d2b4a5305b594c674c97f21fa4cd3a6230efbe1
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-09-24 10:39:02 +0300

    Update main.tf

--- workspaces/dev/main.tf
+++ workspaces/dev/main.tf
@@ -1,7 +1,10 @@
-module "my_module" {
-    source  = "../modules/security-group"
- }
-
+# module "my_module" {
+#     source  = "../modules/security-group"
+#  }
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-terraform-network"
+  auto_create_subnetworks = "true"
+}
 
 provider "google" {
   version = "~> 3.39.0"
